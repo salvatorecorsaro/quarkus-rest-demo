dc4121f5f1747f31763825bf3d3175e481846365
scorsaro <salvatore.corsaro@live.com>
2021-07-06 12:02:35 +0200

    initial

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Durian');
+INSERT INTO fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Strawberry');
+INSERT INTO fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Orange');
